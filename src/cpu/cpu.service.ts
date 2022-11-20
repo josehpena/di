1f479b7d1799287d34a7abaffb5abe206027c6c8
Jose Pena <>
2022-11-20 17:23:38 -0300

    DI Working

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -5,5 +5,9 @@ import { PowerService } from 'src/power/power.service';
 export class CpuService {
     constructor(private powerService: PowerService) { }
 
-
+    compute(a: number, b: number) {
+        console.log('Drawing 10 watts of Power Service');
+        this.powerService.supplyPower(10);
+        return a + b;
+    }
 }
